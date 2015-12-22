1fa51404149d09fb4c27bbdaa84174c43ccf46c3
Aaron Schlesinger <arschles@gmail.com>
2015-12-22 15:26:02 -0800

    fix(releases_test.go): use shorter matcher names

--- _tests/releases_test.go
+++ _tests/releases_test.go
@@ -1,7 +1,6 @@
 package _tests_test
 
 import (
-	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -11,20 +10,20 @@ var _ = Describe("Releases", func() {
 	Context("with no app", func() {
 		It("can create an app", func() {
 			output, err := execute("deis apps:create %s --no-remote", appName)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(output).To(ContainSubstring(fmt.Sprintf("Creating Application... done, created %s", appName)))
+			Expect(err).To(BeNil())
+			Expect(output).Should(HavePrefix("Creating Application... done, created %s", appName))
 		})
 		It("can deploy the app", func() {
 			output, err := execute("deis pull deis/example-go -a %s", appName)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(output).To(ContainSubstring("Creating build... done"))
+			Expect(err).To(BeNil())
+			Expect(output).To(ContainSubstring(HavePrefix("Creating build... done")))
 		})
 	})
 
 	Context("with a deployed app", func() {
 		It("can list releases", func() {
 			output, err := execute("deis releases:list -a %s", appName)
-			Expect(err).NotTo(HaveOccurred())
+			Expect(err).To(BeNil())
 			Expect(output).To(SatisfyAll(
 				HavePrefix("=== %s Releases", appName),
 				MatchRegexp(`v1\t.*\t%s created initial release`, testUser),
@@ -34,7 +33,7 @@ var _ = Describe("Releases", func() {
 
 		It("can rollback to a previous release", func() {
 			output, err := execute("deis releases:rollback v1 -a %s", appName)
-			Expect(err).NotTo(HaveOccurred())
+			Expect(err).To(BeNil())
 			Expect(output).To(SatisfyAll(
 				HavePrefix("Rolling back to"),
 				ContainSubstring("...done"),
@@ -43,7 +42,7 @@ var _ = Describe("Releases", func() {
 
 		It("can get info on releases", func() {
 			output, err := execute("deis releases:info v1 -a %s", appName)
-			Expect(err).NotTo(HaveOccurred())
+			Expect(err).To(BeNil())
 			Expect(output).To(SatisfyAll(
 				HavePrefix("=== %s Release v1", appName),
 				MatchRegexp(`config:\s+[\w-]+`),
