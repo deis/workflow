0a2fb3f2abe1ca250663fb81ab97449457770c4e
Aaron Schlesinger <arschles@gmail.com>
2015-12-22 13:39:57 -0800

    fix(releases_test.go): uncomment integration tests for releases

--- _tests/releases_test.go
+++ _tests/releases_test.go
@@ -1,41 +1,57 @@
 package _tests_test
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Releases", func() {
-	Context("with a deployed app", func() {
+	appName := getRandAppName()
+	Context("with no app", func() {
+		It("can create an app", func() {
+			output, err := execute("deis apps:create %s --no-remote", appName)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(ContainSubstring(fmt.Sprintf("Creating Application... done, created %s", appName)))
+		})
+		It("can deploy the app", func() {
+			output, err := execute("deis pull deis/example-go -a %s", appName)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(ContainSubstring("Creating build... done"))
+		})
+	})
 
-		// XIt("can list releases", func() {
-		// 	output, err := execute("deis releases:list --app=%s", appName)
-		// 	Expect(err).NotTo(HaveOccurred())
-		// 	Expect(output).To(SatisfyAll(
-		// 		HavePrefix("=== %s Releases", appName),
-		// 		MatchRegexp(`v1\t.*\t%s created initial release`, username),
-		// 		MatchRegexp(`v2\t.*\t%s deployed \w+`, username)))
-		// })
-		//
-		// XIt("can rollback to a previous release", func() {
-		// 	output, err := execute("deis releases:rollback v2 --app=%s", appName)
-		// 	Expect(err).NotTo(HaveOccurred())
-		//
-		// 	Expect
-		// 	regexp := `asdf`
-		// 	Expect(output)
-		// })
-		//
-		// XIt("can get info on releases", func() {
-		// 	output, err := execute("deis releases:info %s --app=%s", version, appName)
-		// 	Expect(err).NotTo(HaveOccurred())
-		// 	Expect(output).To(SatisfyAll(
-		// 		HavePrefix("=== %s Release v2", appName),
-		// 		MatchRegexp(`build:\s+[\w-]+`),
-		// 		MatchRegexp(`config:\s+[\w-]+`),
-		// 		MatchRegexp(`owner:\s+%s`, username),
-		// 		MatchRegexp(`summary:\s+%s deployed \w+`, username),
-		// 		MatchRegexp(`uuid:\s+[\w-]+`)))
-		// })
+	Context("with a deployed app", func() {
+		It("can list releases", func() {
+			output, err := execute("deis releases:list -a %s", appName)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(SatisfyAll(
+				HavePrefix("=== %s Releases", appName),
+				MatchRegexp(`v1\t.*\t%s created initial release`, testUser),
+				MatchRegexp(`v2\t.*\t%s deployed \w+`, testUser)),
+			)
+		})
+
+		It("can rollback to a previous release", func() {
+			output, err := execute("deis releases:rollback v1 -a %s", appName)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(SatisfyAll(
+				HavePrefix("Rolling back to"),
+				ContainSubstring("...done"),
+			))
+		})
+
+		It("can get info on releases", func() {
+			output, err := execute("deis releases:info v1 -a %s", appName)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(SatisfyAll(
+				HavePrefix("=== %s Release v1", appName),
+				MatchRegexp(`config:\s+[\w-]+`),
+				MatchRegexp(`owner:\s+%s`, testUser),
+				MatchRegexp(`summary:\s+%s \w+`, testUser),
+				// the below updated date has to match a string like 2015-12-22T21:20:31UTC
+				MatchRegexp(`updated:\s+[0-9]+-[0-9]+-[A-Z0-9]+:[0-9]+:[A-Z0-9:-]+`, appName),
+				MatchRegexp(`uuid:\s+[\w-]+`)))
+		})
 	})
 })
