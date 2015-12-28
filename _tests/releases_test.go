4450d29729a9c51a24e5a2d04a75ba267f23fcad
Aaron Schlesinger <arschles@gmail.com>
2015-12-28 14:21:27 -0800

    ref(releases_test.go): use gexec
    instead of execute

--- _tests/releases_test.go
+++ _tests/releases_test.go
@@ -3,54 +3,56 @@ package _tests_test
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
 )
 
 var _ = Describe("Releases", func() {
 	appName := getRandAppName()
 	Context("with no app", func() {
 		It("can create an app", func() {
-			output, err := execute("deis apps:create %s --no-remote", appName)
+			sess, err := start("deis apps: create %s --no-remote", appName)
 			Expect(err).To(BeNil())
-			Expect(output).Should(HavePrefix("Creating Application... done, created %s", appName))
+			Eventually(sess).Should(gexec.Exit(0))
+			Eventually(sess).Should(gbytes.Say("Creating Application... done, created %s", appName))
 		})
 		It("can deploy the app", func() {
-			output, err := execute("deis pull deis/example-go -a %s", appName)
+			sess, err := start("deis pull deis/example-go -a %s", appName)
 			Expect(err).To(BeNil())
-			Expect(output).To(ContainSubstring("Creating build... done"))
+			Eventually(sess).Should(gexec.Exit(0))
+			Eventually(sess).Should(gbytes.Say("Creating build... done"))
 		})
 	})
 
 	Context("with a deployed app", func() {
 		It("can list releases", func() {
-			output, err := execute("deis releases:list -a %s", appName)
+			sess, err := start("deis releases:list -a %s", appName)
 			Expect(err).To(BeNil())
-			Expect(output).To(SatisfyAll(
-				HavePrefix("=== %s Releases", appName),
-				MatchRegexp(`v1\t.*\t%s created initial release`, testUser),
-				MatchRegexp(`v2\t.*\t%s deployed \w+`, testUser)),
-			)
+			Eventually(sess).Should(gexec.Exit(0))
+			Eventually(sess).Should(gbytes.Say("=== %s Releases", appName))
+			Eventually(sess).Should(gbytes.Say(`v1\t.*\t%s created initial release`, testUser))
+			Eventually(sess).Should(gbytes.Say(`v2\t.*\t%s deployed \w+`, testUser))
 		})
 
 		It("can rollback to a previous release", func() {
-			output, err := execute("deis releases:rollback v1 -a %s", appName)
+			sess, err := start("deis releases:rollback v1 -a %s", appName)
 			Expect(err).To(BeNil())
-			Expect(output).To(SatisfyAll(
-				HavePrefix("Rolling back to"),
-				ContainSubstring("...done"),
-			))
+			Eventually(sess).Should(gexec.Exit(0))
+			Eventually(sess).Should(gbytes.Say("Rolling back to"))
+			Eventually(sess).Should(gbytes.Say("...done"))
 		})
 
 		It("can get info on releases", func() {
-			output, err := execute("deis releases:info v1 -a %s", appName)
+			sess, err := start("deis releases:info v1 -a %s", appName)
 			Expect(err).To(BeNil())
-			Expect(output).To(SatisfyAll(
-				HavePrefix("=== %s Release v1", appName),
-				MatchRegexp(`config:\s+[\w-]+`),
-				MatchRegexp(`owner:\s+%s`, testUser),
-				MatchRegexp(`summary:\s+%s \w+`, testUser),
-				// the below updated date has to match a string like 2015-12-22T21:20:31UTC
-				MatchRegexp(`updated:\s+[0-9]+-[0-9]+-[A-Z0-9]+:[0-9]+:[A-Z0-9:-]+`, appName),
-				MatchRegexp(`uuid:\s+[\w-]+`)))
+			Eventually(sess).Should(gexec.Exit(0))
+			Eventually(sess).Should(gbytes.Say("=== %s Release v1", appName))
+			Eventually(sess).Should(gbytes.Say(`config:\s+[\w-]+`))
+			Eventually(sess).Should(gbytes.Say(`owner:\s+%s`, testUser))
+			Eventually(sess).Should(gbytes.Say(`summary:\s+%s \w+`, testUser))
+			// the below updated date has to match a string like 2015-12-22T21:20:31UTC
+			Eventually(sess).Should(gbytes.Say(`updated:\s+[0-9]+-[0-9]+-[A-Z0-9]+:[0-9]+:[A-Z0-9:-]+`, appName))
+			Eventually(sess).Should(gbytes.Say(`uuid:\s+[\w-]+`))
 		})
 	})
 })
