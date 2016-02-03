084d4d4a28f7d15f57b3320e7f56245ea71a782d
Matt Boersma <mboersma@deis.com>
2016-02-03 11:48:18 -0700

    tests(users): verify with and without slashes

--- rootfs/api/tests/test_users.py
+++ rootfs/api/tests/test_users.py
@@ -11,23 +11,20 @@ class TestUsers(TestCase):
     fixtures = ['tests.json']
 
     def test_super_user_can_list(self):
-        url = '/v2/users'
-
         user = User.objects.get(username='autotest')
         token = Token.objects.get(user=user)
 
-        response = self.client.get(url,
-                                   HTTP_AUTHORIZATION='token {}'.format(token))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data['results']), 3)
+        for url in ['/v2/users', '/v2/users/']:
+            response = self.client.get(url,
+                                       HTTP_AUTHORIZATION='token {}'.format(token))
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(response.data['results']), 3)
 
     def test_non_super_user_cannot_list(self):
-        url = '/v2/users'
-
         user = User.objects.get(username='autotest2')
         token = Token.objects.get(user=user)
 
-        response = self.client.get(url,
-                                   HTTP_AUTHORIZATION='token {}'.format(token))
-        self.assertEqual(response.status_code, 403)
+        for url in ['/v2/users', '/v2/users/']:
+            response = self.client.get(url,
+                                       HTTP_AUTHORIZATION='token {}'.format(token))
+            self.assertEqual(response.status_code, 403)
